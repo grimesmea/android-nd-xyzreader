4c91d74775282f004c6f76579c9ee4c1eca1730a
mary <grimesmea@gmail.com>
2016-02-23 15:11:41 -0500

    Reformat code Upgrade to newest version of OkHttp to fix images not loading

--- XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -2,17 +2,16 @@ package com.example.xyzreader.remote;
 
 import android.util.Log;
 
-import com.squareup.okhttp.OkHttpClient;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONTokener;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class RemoteEndpointUtil {
     private static final String TAG = "RemoteEndpointUtil";
@@ -45,28 +44,14 @@ public class RemoteEndpointUtil {
     }
 
     static String fetchPlainText(URL url) throws IOException {
-        return new String(fetch(url), "UTF-8" );
-    }
+        OkHttpClient client = new OkHttpClient();
 
-    static byte[] fetch(URL url) throws IOException {
-        InputStream in = null;
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
 
-        try {
-            OkHttpClient client = new OkHttpClient();
-            HttpURLConnection conn = client.open(url);
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            in = conn.getInputStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) > 0) {
-                out.write(buffer, 0, bytesRead);
-            }
-            return out.toByteArray();
+        Response response = client.newCall(request).execute();
 
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-        }
+        return response.body().string();
     }
 }
